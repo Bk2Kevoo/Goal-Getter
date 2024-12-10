0081a0475e5aa4cbb68484f913dccb46de3ba539
+ <+>
2024-12-10 18:38:54 -0500

    Finished models and validations

--- server/models/category.py
+++ server/models/category.py
@@ -1,4 +1,50 @@
-from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
+from models.__init__ import SerializerMixin, validates, db
 
-from config import db
+
+class Category(db.Model, SerializerMixin):
+    __tablename__ = "categories"
+
+    id = db.Column(db.Integer, primary_key=True)
+    name = db.Column(db.String, nullable=False)
+    description = db.Column(db.String)
+    allocated_amount = db.Column(db.Integer, nullable=False)
+    created_at = db.Column(db.DateTime, server_default=db.func.now())
+    updated_at = db.Column(db.DateTime, onupdate=db.func.now())
+
+    def __repr__(self):
+        return f"""
+            <Category #{self.id}:
+                Name: {self.name}
+                Description: {self.description}
+                Allocated Amount: {self.allocated_amount}
+        """
+    # Relationship
+    budgets = db.relationship("Budget", back_populates="category", cascade="all, delete-orphan")
+
+    # Serialize
+    serialize_rules = ("-budgets",)
+
+    @validates("name")
+    def validate_name(self, key, name):
+        if not isinstance(name, str):
+            raise TypeError("Name must be a string.")
+        if not (2 <= len(name) <= 50):
+            raise ValueError("Name must be between 2 and 50 characters long.")
+        return name
+    
+    @validates("description")
+    def validate_description(self, key, description):
+        if not isinstance(description, str):
+            raise TypeError("Description must be a string.")
+        description = description.strip()  # Remove leading and trailing whitespace
+        if not (2 <= len(description) <= 255):
+            raise ValueError("Description must be between 2 and 255 characters long.")
+        return description
+    
+    @validates("allocated_amount")
+    def validate_amount(self, key, amount):
+        if not isinstance(amount, int):
+            raise TypeError("Allocated Amount must be of type integer.")
+        if amount < 0:
+            raise ValueError("Allocated Amount cannot be negative.")
+        return amount
--- server/models/goal.py
+++ server/models/goal.py
@@ -1,4 +1,62 @@
-from sqlalchemy_serializer import SerializerMixin
-from sqlalchemy.ext.associationproxy import association_proxy
+from models.__init__ import SerializerMixin, validates, db
+from models.user import User
 
-from config import db
+
+class Goal(db.Model, SerializerMixin):
+    __tablename__ = "goals"
+
+    id = db.Column(db.Integer, primary_key=True)
+    goal_amount = db.Column(db.Float, nullable=False)
+    current_savings = db.Column(db.Float, default=0.0, nullable=False)
+    is_completed = db.Column(db.Boolean, default=False) 
+    date = db.Column(db.Date, nullable=False)
+    user_id = db.Column(db.Integer, db.ForeignKey("users.id"), nullable=False)
+
+    def __repr__(self):
+        return f"""
+            <Goal #{self.id}:
+                Goal_Amount: {self.goal_amount}
+                Current Savings: {self.current_savings}
+                Is Completed: {self.is_completed}
+                Date: {self.date}
+                User Id: {self.user_id}
+        """
+
+    # Relationships
+    user = db.relationship("User", back_populates="goals") 
+
+    # Serialize
+    serialize_rules = ("-user",)
+
+    
+    @validates("goal_amount")
+    def validate_goal_amount(self, key, value):
+        if value <= 0:
+            raise ValueError("Goal amount must be greater than zero.")
+        return value
+
+    @validates("current_savings")
+    def validate_current_savings(self, key, value):
+        if value < 0:
+            raise ValueError("Current savings cannot be negative.")
+        elif hasattr(self, "goal_amount") and value > self.goal_amount:
+            raise ValueError("Current savings cannot exceed the goal amount.")
+        return value
+    
+    @validates("date")
+    def validate_date(self, key, value):
+        if not value:
+            raise ValueError("A valid date must be provided.")
+        return value
+
+    @validates("user_id")
+    def validate_user_id(self, _, user_id):
+        if not isinstance(user_id, int):
+            raise TypeError("Category ids must be integers")
+        elif user_id < 1:
+            raise ValueError(
+                f"{user_id} has to be a positive integer"
+            )
+        elif not db.session.get(User, user_id):
+            raise ValueError(f"{user_id} must belong to an existing User")
+        return user_id
